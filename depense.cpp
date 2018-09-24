e296e51ef9bc6e6fda03f9ae3345e6f0d553f2d3
KadunaPoly <43528902+KadunaPoly@users.noreply.github.com>
2018-09-23 23:33:12 -0400

    creation de l'entete depense

--- depense.cpp
+++ depense.cpp
@@ -1,3 +1,13 @@
+/****************************************************************************************************/
+/* Programme : TP1-Depenses.cpp                     						    */
+/*                                                                       			    */
+/* Objectif     : creer les classes et des differentes methodes  			            */
+/*                                                                    				    */
+/* Auteur    : Karl Nelson Somo 1859229 et Karl Dunkelmann 1893306 		                    */
+/* Date      : 23/09/2018                            					            */
+/****************************************************************************************************/
+
+
 #include "depense.h"
 
 Depense::Depense(): titre_("unknown"), montant_(0.0) {
